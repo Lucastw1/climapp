63e0bfd0ce510c4e9461d826b580b1dc91829489
Lucas <luquillasojeda@gmail.com>
2024-06-03 16:13:10 -0300

    First commit

---
+++ climapp/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Navbar from './components/navbar'
+
+
+function App() {
+  
+
+  return (
+    <>
+    <div><Navbar></Navbar></div>
+    </>
+  )
+}
+
+export default App
